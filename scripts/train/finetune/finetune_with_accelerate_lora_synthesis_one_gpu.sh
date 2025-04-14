4d3f8f191bb5b531c82c88d526cc3ad9ad023cea
Arthur Chen <chenhaonan189@gmail.com>
2025-04-14 00:28:16 -0400

    Add new finetuning script for single GPU training with LoRA and Accelerate. Updated existing finetune script to correct flash attention flag and improve configuration settings for better performance and logging integration with wandb.

--- scripts/train/finetune/finetune_with_accelerate_lora_synthesis_one_gpu.sh
+++ scripts/train/finetune/finetune_with_accelerate_lora_synthesis_one_gpu.sh
@@ -1,15 +1,11 @@
 # input arguments
-TRAIN_FILE=$1
-OUTPUT_DIR=$2
-PORTION=$3
-SOURCE=$4
-PORT=$5
+OUTPUT_DIR=/projects/r2llab/arthur/checkpoints/train_synthesizer
 
 # wandb configuration
 source secrets.sh
-WANDB_NAME=Qwen2.5-7B-Instruct_nnetnav_${SOURCE}_disjoint_portion${PORTION}_lora
+WANDB_NAME=Qwen2.5-1.5B-Instruct_synthesis
 export WANDB_NAME=${WANDB_NAME:0:64} # this is to avoid wandb error
-export WANDB_PROJECT=data-synthesis-evaluation-nnetnav-disjoint
+export WANDB_PROJECT=data-synthesis-training
 
 # huggingface configuration
 export HF_REPO_ID=$WANDB_NAME
@@ -17,11 +13,10 @@ export HF_REPO_REVISION=main
 export HF_HOME=$HOME/.cache/huggingface
 
 # configuration
-OUTPUT_DIR="$2/qwen2.5-7b-instruct_nnetnav_${SOURCE}_disjoint_portion${PORTION}_lora"
-NUM_GPUS=2
-BATCH_SIZE_PER_GPU=1
-TOTAL_BATCH_SIZE=32
-MODEL_AND_TOKENIZER_NAME=Qwen/Qwen2.5-7B-Instruct
+NUM_GPUS=1
+BATCH_SIZE_PER_GPU=2
+TOTAL_BATCH_SIZE=2
+MODEL_AND_TOKENIZER_NAME=Qwen/Qwen2.5-1.5B-Instruct
 SCRIPT=$HOME/Workspace/open-instruct/open_instruct/finetune.py
 DATASET_LOCAL_CACHE_DIR="$HOME/.cache"
 GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE/$NUM_GPUS/$BATCH_SIZE_PER_GPU))
@@ -32,18 +27,19 @@ echo "Training ${MODEL_AND_TOKENIZER_NAME} using $NUM_GPUS GPUs, $BATCH_SIZE_PER
 # but it will trade off speed.
 
 accelerate launch \
-    --config_file configs/ds_configs/stage3_qlora.yaml \
-    --main_process_port $PORT \
+    --mixed_precision bf16 \
+    --num_processes 1 \
     $SCRIPT \
     --model_name_or_path $MODEL_AND_TOKENIZER_NAME \
-    --use_lora \
     --use_flash_attn \
-    --max_seq_length 20000 \
+    --use_liger_kernel \
+    --max_seq_length 1536 \
     --preprocessing_num_workers 128 \
     --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
     --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
     --learning_rate 2e-5 \
     --lr_scheduler_type linear \
+    --dataset_mixer_list ArthurChen189/financial_news_sentiment_analysis_sft 1.0 \
     --warmup_ratio 0.03 \
     --weight_decay 0.0 \
     --num_train_epochs $EPOCHS \
